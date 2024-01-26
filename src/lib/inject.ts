0f321217b51bccaaf3316fa58adba5e20e3b61b8
Roman <roman_morokhovskyi@goodyear.com>
2024-01-26 23:34:31 +0200

    add README.md

--- src/lib/inject.ts
+++ src/lib/inject.ts
@@ -1,7 +1,13 @@
 import { getInjectable } from "./injectable";
 
 export function Inject(injectionKey: string, ...args: any[]) {
-    return function applyInjection(_property: any, _context: any) {
-        return () => getInjectable(injectionKey, args);
+    return function applyInjection(
+        _property: any,
+        context: ClassFieldDecoratorContext,
+    ) {
+        if (context.kind === "field") {
+            return () => getInjectable(injectionKey, args);
+        }
+        return;
     };
 }
